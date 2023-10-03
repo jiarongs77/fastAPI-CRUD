73df5ae600cb40c55ffc4942130c0854cee6bde1
Zhaolong Zhong <zhlgzh@gmail.com>
2023-10-03 11:48:32 -0700

    Fixed tests warnings

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,8 @@
 import secrets
 from typing import Any, Dict, List, Optional, Union
 
-from pydantic import AnyHttpUrl, EmailStr, HttpUrl, PostgresDsn, validator
-from pydantic_settings import BaseSettings
+from pydantic import AnyHttpUrl, EmailStr, HttpUrl, PostgresDsn, ValidationInfo, field_validator, ConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 # https://docs.pydantic.dev/latest/concepts/pydantic_settings/
@@ -24,7 +24,7 @@ class Settings(BaseSettings):
     FIRST_SUPERUSER: Optional[EmailStr] = None
     FIRST_SUPERUSER_PASSWORD: Optional[str] = None
 
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
@@ -40,8 +40,10 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     SQLALCHEMY_DATABASE_URI: Optional[str] = None
 
-    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
-    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+    @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
+    def assemble_db_connection(cls, v: Optional[str], info: ValidationInfo) -> Any:
+        assert info.data is not None
+        values = info.data
         if isinstance(v, str):
             return v
         return str(
@@ -69,11 +71,11 @@ class Settings(BaseSettings):
     EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
     USERS_OPEN_REGISTRATION: bool = True  # Allow public registration or not
 
-    # model_config = SettingsConfigDict(env_file="./app/.env", env_file_encoding="utf-8")
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-        from_attributes = True
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        env_file_encoding = "utf-8",
+        from_attributes = True,
+        extra='ignore')
 
 
 settings = Settings()
--- app/database.py
+++ app/database.py
@@ -1,15 +1,13 @@
 from typing import Any
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import sessionmaker, as_declarative
 
 from app.core.config import settings
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
 
 
 @as_declarative()
